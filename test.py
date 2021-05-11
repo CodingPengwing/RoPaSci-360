4b2ce1aa00c2702bf58c4d3ff0650c9ef8a17bdd
Jennifer Xiang <jennifer.xiang@student.unimelb.edu.au>
2021-05-11 16:41:46 +1000

    lolol testtest

---
+++ test.py
@@ -0,0 +1,30 @@
+fileHandle = open ( 'output.txt',"r" )
+lineList = fileHandle.readlines()
+fileHandle.close()
+
+
+result = lineList[-1]
+
+fileHandle = open ( 'result.txt',"r" )
+lineList = fileHandle.readlines()
+fileHandle.close()
+
+upper_wins = int(lineList[0].split(',')[1])
+draws = int(lineList[1].split(',')[1])
+lower_wins = int(lineList[2].split(',')[1])
+
+if "upper" in result:
+    upper_wins += 1
+elif "lower" in result:
+    lower_wins += 1
+elif "draw" in result:
+    draws += 1
+else:
+    print("ERROR")
+
+f = open ( 'result.txt',"w" )
+f.write("upper_wins," + str(upper_wins)+"\n")
+f.write("draws," + str(draws)+"\n")
+f.write("lower_wins," + str(lower_wins)+"\n")
+f.close()
+
